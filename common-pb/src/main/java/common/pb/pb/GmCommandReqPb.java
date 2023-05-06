de9fa50c48635461a311e6595a9c242bd3a63d72
yuanshuangli <yuanshuangli@shoyoo.com>
2023-05-06 13:55:03 +0800

    proto update

--- common-pb/src/main/java/common/pb/pb/GmCommandReqPb.java
+++ common-pb/src/main/java/common/pb/pb/GmCommandReqPb.java
@@ -22,25 +22,18 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PUBLIC)
 @ProtoFileMerge(fileName = ProtoFile.COMMON_FILE_NAME, filePackage = ProtoFile.COMMON_FILE_PACKAGE)
 public class GmCommandReqPb {
-    /** gmCommandId
-     * 1:加钱
-     * 2:装备全解锁命令
-     * 3:载具全解锁命令
-     * 4:富豪全解锁
-     * 5:boss全解锁
-     * 6:属性升级
-     **/
+    /** gmCommandId **/
+    // 1:加钱 2:装备全解锁命令 3:载具全解锁命令 4:富豪全解锁 5:boss全解锁 6:属性升级
     @NotNull
     int gmCommandId;
 
-    /** gm命令对应的信息
-     * 加钱命令:{"money",10}
-     * 装备全解锁命令:{}
-     * 载具全解锁命令:{}
-     * 富豪全解锁:{}
-     * boss全解锁:{}
-     * 属性升级:{"attributeType":1,"targetLevel":1000}
-     **/
+    /** gm命令对应的信息 **/
+    // 加钱命令:{"money",10}
+    // 装备全解锁命令:{}
+    // 载具全解锁命令:{}
+    // 富豪全解锁:{}
+    // boss全解锁:{}
+    // 属性升级:{"attributeType":1,"targetLevel":1000}
     @NotNull
     String jsonGmCommandInfo;
 }
